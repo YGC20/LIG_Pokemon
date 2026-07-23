f274f4285bf0ebd99b2db634dd93493bfbdead17
YGC20 <126743685+YGC20@users.noreply.github.com>
2026-07-23 17:44:48 +0900

    feat: 포켓몬 스킬 메뉴 버튼 생성

---
+++ Pokemon/Page/PokemonButtonPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pokemon.Page
+{
+    /// <summary>
+    /// PokemonButtonPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PokemonButtonPage : System.Windows.Controls.Page
+    {
+        public PokemonButtonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Pokemon1ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Pokemon2ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Pokemon3ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Pokemon4ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MenuButtonPage());
+        }
+
+        
+    }
+}
---
+++ Pokemon/Page/SkillButtonPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pokemon.Page
+{
+    /// <summary>
+    /// SkillButtonPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SkillButtonPage : System.Windows.Controls.Page
+    {
+        public SkillButtonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Skill1ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Skill2ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Skill3ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Skill4ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MenuButtonPage());
+        }
+    }
+}
