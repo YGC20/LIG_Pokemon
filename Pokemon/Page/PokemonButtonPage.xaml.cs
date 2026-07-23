ae795eb191877427f77196d4b522871a3c32b722
YGC20 <126743685+YGC20@users.noreply.github.com>
2026-07-23 17:22:04 +0900

    feat: Front 작업 진행
    feat: Front 작업 진행
    
    - 메인 페이지 작업
    - 보스 선택 페이지 작업
    - 배틀 페이지 작업

--- Pokemon/Page/PokemonButtonPage.xaml.cs
+++ Pokemon/Page/PokemonButtonPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,17 +11,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Pokemon
+namespace Pokemon.Page
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// PokemonButtonPage.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PokemonButtonPage : System.Windows.Controls.Page
     {
-        public MainWindow()
+        public PokemonButtonPage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new Uri("Page/MenuPage.xaml", UriKind.Relative));
         }
     }
 }
