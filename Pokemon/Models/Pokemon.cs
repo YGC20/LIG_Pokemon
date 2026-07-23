2c1a44a5b046feb6d5bbf68c48300c42fd8c66c3
inseong276-creator <inseong276@gmail.com>
2026-07-23 15:44:04 +0900

    add Models

---
+++ Pokemon/Models/Pokemon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon.Models
+{
+
+
+    namespace Pokemon
+    {
+        public class Pokemon
+        {
+            public int Id { get; }
+            public string Name { get; }
+            public PokemonType Type { get; }
+
+            public int MaxHp { get; }
+            public int CurrentHp { get; private set; }
+
+            public int Attack { get; }
+            public int Defense { get; }
+
+            public IReadOnlyList<Skill> Skills { get; }
+
+            public string FrontImagePath =>
+                $"/Assets/Pokemon/Front/{Id}.png";
+
+            public string BackImagePath =>
+                $"/Assets/Pokemon/Back/{Id}.png";
+
+            public bool IsFainted => CurrentHp <= 0;
+
+            public Pokemon(
+                int id,
+                string name,
+                PokemonType type,
+                int maxHp,
+                int attack,
+                int defense,
+                IEnumerable<Skill> skills)
+            {
+                Id = id;
+                Name = name;
+                Type = type;
+
+                MaxHp = maxHp;
+                CurrentHp = maxHp;
+
+                Attack = attack;
+                Defense = defense;
+
+                Skills = skills.ToList();
+            }
+
+            public void TakeDamage(int damage)
+            {
+                CurrentHp = Math.Max(0, CurrentHp - damage);
+            }
+        }
+    }
+}
---
+++ Pokemon/Models/PokemonType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon
+{
+    public enum PokemonType
+    {
+        Normal,
+        Fire,
+        Water,
+        Electric,
+        Grass,
+        Ice,
+        Fighting,
+        Poison,
+        Ground,
+        Flying,
+        Psychic,
+        Bug,
+        Rock,
+        Ghost,
+        Dragon,
+        Dark,
+        Steel,
+        Fairy,
+    }
+}
